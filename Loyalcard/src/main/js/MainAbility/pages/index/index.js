0f18b82c2d3d0a42361516876e0afe8122aa5a2d
Michal Uhrin <uhrin.michal@gmail.com>
2026-06-29 14:24:22 +0200

    refactor: prepare foundation for card model

--- Loyalcard/src/main/js/MainAbility/pages/index/index.js
+++ Loyalcard/src/main/js/MainAbility/pages/index/index.js
@@ -1,26 +1,63 @@
 export default {
   data: {
-    currentName: '',
-    currentCode: ''
+    viewMode: 'list',
+    selectedName: '',
+    selectedType: '',
+    selectedCode: '',
+
+    cards: [
+      {
+        id: 'lidl',
+        name: 'Lidl Plus',
+        type: 'QR karta',
+        code: '1234567890123'
+      },
+      {
+        id: 'tesco',
+        name: 'Tesco Clubcard',
+        type: 'Ciarovy kod',
+        code: '9876543210123'
+      },
+      {
+        id: 'kaufland',
+        name: 'Kaufland Card',
+        type: 'QR karta',
+        code: '555666777888'
+      },
+      {
+        id: 'billa',
+        name: 'BILLA Bonus',
+        type: 'Ciarovy kod',
+        code: '444333222111'
+      }
+    ]
   },
 
-  showLidl() {
-    this.currentName = 'Lidl Plus';
-    this.currentCode = '1234567890123';
+  openCard(index) {
+    const card = this.cards[index];
+    this.selectedName = card.name;
+    this.selectedType = card.type;
+    this.selectedCode = card.code;
+    this.viewMode = 'detail';
   },
 
-  showTesco() {
-    this.currentName = 'Tesco Clubcard';
-    this.currentCode = '9876543210123';
+  openLidl() {
+    this.openCard(0);
   },
 
-  showKaufland() {
-    this.currentName = 'Kaufland Card';
-    this.currentCode = '555666777888';
+  openTesco() {
+    this.openCard(1);
   },
 
-  showBilla() {
-    this.currentName = 'BILLA Bonus';
-    this.currentCode = '444333222111';
+  openKaufland() {
+    this.openCard(2);
+  },
+
+  openBilla() {
+    this.openCard(3);
+  },
+
+  goBack() {
+    this.viewMode = 'list';
   }
 }
