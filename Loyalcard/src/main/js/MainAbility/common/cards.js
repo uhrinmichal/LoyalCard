07f6516a094590376db007505cd0ebeeb91824ef
Michal Uhrin <uhrin.michal@gmail.com>
2026-06-29 14:45:43 +0200

    refactor: introduce card model

---
+++ Loyalcard/src/main/js/MainAbility/common/cards.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    name: "Lidl Plus",
+    type: "EAN13",
+    code: "8588001234567",
+    color: "yellow"
+  },
+  {
+    id: 2,
+    name: "Tesco Clubcard",
+    type: "EAN13",
+    code: "9799500025434644",
+    color: "blue"
+  },
+  {
+    id: 3,
+    name: "Kaufland Card",
+    type: "QR",
+    code: "6002110627203614",
+    color: "orange"
+  }
+]
